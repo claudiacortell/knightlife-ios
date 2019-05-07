e2d1e655231ccc5d2f26dafaa1029c3e57fc264d
JosephZhou <josephzhou1234@gmail.com>
2019-05-06 23:22:47 -0400

    finished modified cell
    finished the look for the modified cell, half-finished with mirroring code from blockcell onto calendar cell

--- Glancer/CalendarCell.swift
+++ Glancer/CalendarCell.swift
@@ -1,53 +1,49 @@
 //
-//  BlockCell.swift
+//  CalendarCell.swift
 //  Glancer
 //
-//  Created by Dylan Hanson on 7/25/18.
-//  Copyright © 2018 Dylan Hanson. All rights reserved.
+//  Created by Joseph Zhou on 5/5/19.
+//  Copyright © 2019 Dylan Hanson. All rights reserved.
 //
 
 import Foundation
 import UIKit
 import AddictiveLib
 
-class BlockCell: TableCell {
+class CalendarCell: TableCell {
     
-    private let controller: DayController
+    private let controller: CalendarController
     private let composite: CompositeBlock
     
-    init(controller: DayController, composite: CompositeBlock) {
+    init(controller: CalendarController, composite: CompositeBlock) {
         self.controller = controller
         self.composite = composite
         
-        super.init("block", nib: "BlockCell")
+        super.init("calendar", nib: "CalendarCell")
         
-        self.setEstimatedHeight(70)
+        self.setEstimatedHeight(50)
         self.setSelectionStyle(.none)
         
         self.setCallback() {
             template, cell in
             
-            if let blockCell = cell as? UIBlockCell {
-                self.layout(cell: blockCell)
+            if let calendarCell = cell as? UICalendarCell {
+                self.layout(cell: calendarCell)
             }
         }
     }
     
     //    Set name label to bold if there's a class or not
     
-    private func layout(cell: UIBlockCell) {
-        let analyst = BlockAnalyst(schedule: self.composite.schedule, block: self.composite.block)
-        let block = self.composite.block
+    private func layout(cell: UICalendarCell) {
+        let analyst = BlockAnalyst(schedule: self.composite.schedule, calendar: self.composite.calendar)
+        let block = self.composite.calendar
         
         //        Setup
         cell.nameLabel.text = analyst.getDisplayName()
-        cell.blockNameLabel.text = block.id.displayName
         
-        cell.fromLabel.text = block.time.start.prettyTime
-        cell.toLabel.text = block.time.end.prettyTime
-        
-        cell.locationLabel.text = analyst.getLocation()
-        
+        cell.fromLabel.text = calendar.time.start.prettyTime
+
         //        Formatting
         var heavy = !analyst.getCourses().isEmpty
         if block.id == .lab, let before = self.composite.schedule.getBlockBefore(block) {
@@ -59,8 +55,6 @@ class BlockCell: TableCell {
         cell.nameLabel.font = UIFont.systemFont(ofSize: 22, weight: heavy ? .bold : .semibold)
         cell.nameLabel.textColor = analyst.getColor()
         
-        cell.tagIcon.image = cell.tagIcon.image!.withRenderingMode(.alwaysTemplate)
-        cell.rightIcon.image = cell.rightIcon.image!.withRenderingMode(.alwaysTemplate)
         
         //        Attachments
         for arranged in cell.attachmentsStack.arrangedSubviews { cell.attachmentsStack.removeArrangedSubview(arranged) ; arranged.removeFromSuperview() }
@@ -80,30 +74,19 @@ class BlockCell: TableCell {
                 continue // Don't show if it's not relevant
             }
             
-			let view = EventAttachmentView()
-			view.text = event.completeDescription()
-			cell.attachmentsStack.addArrangedSubview(view)
-		}
-		
-		cell.attachmentStackBottomConstraint.constant = cell.attachmentsStack.arrangedSubviews.count > 0 ? 10.0 : 0.0
-	}
-	
+            let view = EventAttachmentView()
+            view.text = event.completeDescription()
+            cell.attachmentsStack.addArrangedSubview(view)
+        }
+        
+        cell.attachmentStackBottomConstraint.constant = cell.attachmentsStack.arrangedSubviews.count > 0 ? 10.0 : 0.0
+    }
+    
 }
 
-class UIBlockCell: UITableViewCell {
-	
-	@IBOutlet weak var nameLabel: UILabel!
-	
-	@IBOutlet weak var tagIcon: UIImageView!
-	@IBOutlet weak var blockNameLabel: UILabel!
-	
-	@IBOutlet weak var fromLabel: UILabel!
-	@IBOutlet weak var rightIcon: UIImageView!
-	@IBOutlet weak var toLabel: UILabel!
-	
-	@IBOutlet weak var attachmentsStack: UIStackView!
-	@IBOutlet weak var attachmentStackBottomConstraint: NSLayoutConstraint!
-	
-	@IBOutlet weak var locationLabel: UILabel!
-	
+class UICalendarCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var fromLabel: UILabel!
+    
 }
---
+++ Glancer/CalenderCell.swift
@@ -0,0 +1,9 @@
+//
+//  CalenderCell.swift
+//  Glancer
+//
+//  Created by Joseph Zhou on 5/5/19.
+//  Copyright © 2019 Dylan Hanson. All rights reserved.
+//
+
+import Foundation
